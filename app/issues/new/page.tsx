5572fcd16ea04168ea8587cfb8b66bf7ff343501
Mohammad-marghoob <marghoob.mohamad1998@gmail.com>
2025-02-08 17:00:35 +0330

    Implementing client side validation

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,21 +1,28 @@
 "use client";
 
 import { useForm, Controller } from "react-hook-form";
-import axios from "axios";
 import { Button, Callout, TextField } from "@radix-ui/themes";
-import SimpleMDE from "react-simplemde-editor";
-import "easymde/dist/easymde.min.css";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { createIssueSchema } from "@/app/ValidationSchemas";
+import { z } from "zod";
+import axios from "axios";
+import SimpleMDE from "react-simplemde-editor";
+import "easymde/dist/easymde.min.css";
 
-interface IssueForm {
-  title: string;
-  description: string;
-}
+type IssueForm = z.infer<typeof createIssueSchema>;
 
 const newIssuePage = () => {
   const router = useRouter();
-  const { register, control, handleSubmit } = useForm<IssueForm>();
+  const {
+    register,
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IssueForm>({
+    resolver: zodResolver(createIssueSchema),
+  });
   const [error, setError] = useState("");
 
   return (
@@ -37,6 +44,11 @@ const newIssuePage = () => {
         })}
       >
         <TextField.Root placeholder="Title" {...register("title")} />
+        {errors.title && (
+          <Callout.Root color="red" className="mb-5">
+            <Callout.Text>{errors.title.message}</Callout.Text>
+          </Callout.Root>
+        )}
         <Controller
           name="description"
           control={control}
@@ -44,6 +56,11 @@ const newIssuePage = () => {
             return <SimpleMDE placeholder="Description" {...field} />;
           }}
         ></Controller>
+        {errors.description && (
+          <Callout.Root color="red" className="mb-5">
+            <Callout.Text>{errors.description.message}</Callout.Text>
+          </Callout.Root>
+        )}
         <Button>Submit New Issue</Button>
       </form>
     </div>
