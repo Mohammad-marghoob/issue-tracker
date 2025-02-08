de9beda114d1580a600092bc647058de74c0c94f
Mohammad-marghoob <marghoob.mohamad1998@gmail.com>
2025-02-08 12:18:21 +0330

    Initial commit after cleanup

---
+++ app/Navbar.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+import Link from "next/link";
+import { ImBug } from "react-icons/im";
+import { usePathname } from "next/navigation";
+
+const NavBar = () => {
+  const currentPath = usePathname();
+
+  const links = [
+    { label: "Dashboard", href: "/" },
+    { label: "Issues", href: "/issues" },
+  ];
+
+  return (
+    <nav className="flex space-x-6 border-b mb-5 px-5 h-14 items-center">
+      <Link href="/">
+        <ImBug />
+      </Link>
+      <ul className="flex space-x-6">
+        {links.map((link) => (
+          <li key={link.href}>
+            <Link
+              className={`
+                ${
+                  link.href === currentPath ? "text-zinc-900" : "text-zinc-500"
+                } 
+                 hover:text-zinc-800 transition-colors`}
+              href={link.href}
+            >
+              {link.label}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+};
+
+export default NavBar;
