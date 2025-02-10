6ee92f461ca3a93cfc3441193a3681d019810ae5
Mohammad-marghoob <marghoob.mohamad1998@gmail.com>
2025-02-10 10:06:32 +0330

    Add the edit button

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,9 +1,11 @@
 import { prisma } from "@/prisma/client";
-import { Card, Flex, Heading, Text } from "@radix-ui/themes";
+import { Box, Button, Card, Flex, Grid, Heading, Text } from "@radix-ui/themes";
 import { notFound } from "next/navigation";
 import { ThickArrowRightIcon } from "@radix-ui/react-icons";
 import IssueStatusBadge from "@/app/components/IssueStatusBadge";
 import Markdown from "react-markdown";
+import { Pencil2Icon } from "@radix-ui/react-icons";
+import Link from "next/link";
 
 interface Props {
   params: { id: string };
@@ -22,19 +24,33 @@ const IssueDetailPage = async ({ params }: Props) => {
     }
 
     return (
-      <div>
-        <Flex align="center" gap="2" my="2">
-          <Heading>{issue.title}</Heading>
-          <ThickArrowRightIcon color="red" />
-          <Text>{issue.createdAt.toDateString()}</Text>
-          <IssueStatusBadge status={issue.status} />
-        </Flex>
-        <Card className="prose">
-          <Text as="div" size="4">
-            <Markdown>{issue.description}</Markdown>
-          </Text>
-        </Card>
-      </div>
+      <Grid
+        columns={{
+          initial: "1",
+          md: "2",
+        }}
+        gap="2"
+      >
+        <Box>
+          <Flex align="center" gap="2" my="2">
+            <Heading>{issue.title}</Heading>
+            <ThickArrowRightIcon color="red" />
+            <Text>{issue.createdAt.toDateString()}</Text>
+            <IssueStatusBadge status={issue.status} />
+          </Flex>
+          <Card className="prose">
+            <Text as="div" size="4">
+              <Markdown>{issue.description}</Markdown>
+            </Text>
+          </Card>
+        </Box>
+        <Box>
+          <Button>
+            <Pencil2Icon />
+            <Link href={`/issues/${issue.id}/edit`}>Edit issue</Link>
+          </Button>
+        </Box>
+      </Grid>
     );
   } catch (error) {
     notFound();
