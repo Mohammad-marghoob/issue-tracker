0413571e89ece0ba4cdab1ad7f7193bac31776f7
Mohammad-marghoob <marghoob.mohamad1998@gmail.com>
2025-02-10 12:38:05 +0330

    Handle Errors

--- app/issues/[id]/DeleteIssueButton.tsx
+++ app/issues/[id]/DeleteIssueButton.tsx
@@ -4,9 +4,20 @@ import { AlertDialog, Button, Flex } from "@radix-ui/themes";
 import { TrashIcon } from "@radix-ui/react-icons";
 import axios from "axios";
 import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 const DeleteIssueButton = ({ issueId }: { issueId: number }) => {
   const router = useRouter();
+  const [error, setError] = useState(false);
+
+  const handleDelete = async () => {
+    try {
+      await axios.delete("/api/issues/" + issueId);
+      router.push("/issues");
+    } catch (error) {
+      setError(true);
+    }
+  };
 
   return (
     <>
@@ -30,20 +41,37 @@ const DeleteIssueButton = ({ issueId }: { issueId: number }) => {
               </Button>
             </AlertDialog.Cancel>
             <AlertDialog.Action>
-              <Button
-                variant="solid"
-                color="red"
-                onClick={async () => {
-                  await axios.delete("/api/issues/" + issueId);
-                  router.push("/issues");
-                }}
-              >
+              <Button variant="solid" color="red" onClick={handleDelete}>
                 Delete
               </Button>
             </AlertDialog.Action>
           </Flex>
         </AlertDialog.Content>
       </AlertDialog.Root>
+
+      {/* Error display block  */}
+      {error && (
+        <AlertDialog.Root open={error}>
+          <AlertDialog.Content maxWidth="450px">
+            <AlertDialog.Title>Error</AlertDialog.Title>
+            <AlertDialog.Description size="2">
+              This issue can not be deleted.
+            </AlertDialog.Description>
+
+            <Flex gap="3" mt="4" justify="end">
+              <AlertDialog.Cancel>
+                <Button
+                  variant="soft"
+                  color="gray"
+                  onClick={() => setError(false)}
+                >
+                  Cancel
+                </Button>
+              </AlertDialog.Cancel>
+            </Flex>
+          </AlertDialog.Content>
+        </AlertDialog.Root>
+      )}
     </>
   );
 };
